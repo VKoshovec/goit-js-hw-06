e481e8fe5886c5d2c19e070e8b0fa9df6e739203
VladimirKoshovec <koshavladmir@gmail.com>
2022-11-01 10:57:00 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -12,22 +12,18 @@ const input = document.querySelector ('#validation-input');
 const inputLength = input.dataset.length;
 
 function checkValue () {
-   if (input.value.length < 6) {
-
-     if (input.classList.contains("valid")){
-       input.classList.replace("valid", "invalid");
-     } else {
-        input.classList.add("invalid");
-     }
-    
+   if (input.value.length === 6) {
+      if (input.classList.contains("invalid")){
+         input.classList.replace("invalid", "valid");
+       } else {
+          input.classList.add("valid");
+       }
    } else {
-
-     if (input.classList.contains("invalid")){
-        input.classList.replace("invalid", "valid");
-      } else {
-         input.classList.add("valid");
-      }
-
+      if (input.classList.contains("valid")){
+         input.classList.replace("valid", "invalid");
+       } else {
+          input.classList.add("invalid");
+       }
    }
 }
 
